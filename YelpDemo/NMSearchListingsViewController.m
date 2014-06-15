f39a8071eae7065b8626dacab88329c57cc6eb45
Nicolas Melo <nicolas@path.com>
2014-06-15 10:22:09 -0400

    Infinite scroll.

--- YelpDemo/NMSearchListingsViewController.m
+++ YelpDemo/NMSearchListingsViewController.m
@@ -18,9 +18,10 @@ NSString * const kYelpTokenSecret = @"mqtKIxMIR4iBtBPZCmCLEb-Dz3Y";
 
 @interface NMSearchListingsViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (strong, nonatomic) IBOutlet UISearchBar *listingsSearchBar;
+@property (strong, nonatomic) UISearchBar *listingsSearchBar;
 @property (strong, nonatomic) YelpClient *client;
 @property (strong, nonatomic) NSMutableArray *listingsArray;
+@property (strong, nonatomic) NSMutableDictionary *searchParameters;
 
 @end
 
@@ -33,6 +34,7 @@ NSString * const kYelpTokenSecret = @"mqtKIxMIR4iBtBPZCmCLEb-Dz3Y";
         self.client = [[YelpClient alloc] initWithConsumerKey:kYelpConsumerKey consumerSecret:kYelpConsumerSecret accessToken:kYelpToken accessSecret:kYelpTokenSecret];
         self.listingsArray = [[NSMutableArray alloc] initWithCapacity:0];
         self.listingsSearchBar = [[UISearchBar alloc] init];
+        self.searchParameters = [[NSMutableDictionary alloc] initWithDictionary:@{@"location": @"San Francisco", @"limit": @(20), @"offset": @(0)}];
     }
     return self;
 }
@@ -56,14 +58,33 @@ NSString * const kYelpTokenSecret = @"mqtKIxMIR4iBtBPZCmCLEb-Dz3Y";
 
 #pragma mark - TableViewController Delegates
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.listingsArray.count;
+    return self.listingsArray.count == 0 ? self.listingsArray.count : self.listingsArray.count + 1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     UITableViewCell *cell = [[UITableViewCell alloc] init];
-    NSDictionary *business = self.listingsArray[indexPath.row];
-    cell.textLabel.text = business[@"name"];
+    
+    if (self.listingsArray.count > 0 && indexPath.row == self.listingsArray.count) {
+        
+        NSInteger offset = self.listingsArray.count;
+        [self.searchParameters setValue:@(offset) forKey:@"offset"];
+        
+        [self.client searchWithTerm:self.searchParameters success:^(AFHTTPRequestOperation *operation, id response) {
+            NSArray *businesses = response[@"businesses"];
+            for (NSDictionary *business in businesses) {
+                [self.listingsArray addObject:business];
+            }
+            [self.tableView reloadData];
+        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+            NSLog(@"error: %@", [error description]);
+        }];
+    }
+    else {
+        NSDictionary *business = self.listingsArray[indexPath.row];
+        cell.textLabel.text = business[@"name"];
+    }
+    
     return cell;
 }
 
@@ -75,7 +96,10 @@ NSString * const kYelpTokenSecret = @"mqtKIxMIR4iBtBPZCmCLEb-Dz3Y";
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)theSearchBar {
     NSLog(@"Search item: %@", theSearchBar.text);
-    [self.client searchWithTerm:theSearchBar.text success:^(AFHTTPRequestOperation *operation, id response) {
+    
+    [self.searchParameters setValue:theSearchBar.text forKey:@"term"];
+    
+    [self.client searchWithTerm:self.searchParameters success:^(AFHTTPRequestOperation *operation, id response) {
         NSArray *businesses = response[@"businesses"];
         for (NSDictionary *business in businesses) {
             [self.listingsArray addObject:business];
